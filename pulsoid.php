3b6fe06f56deb86718018d31320f4ad308c74a07
UnskilledFreak <git@unskilledfreak.de>
2021-10-12 19:27:18 +0200

    fixed pulsoid feed not working SSL peer verify was set

--- pulsoid.php
+++ pulsoid.php
@@ -5,7 +5,15 @@ header("Access-Control-Allow-Origin: *");
 $pulsoidUrl = $_GET['pFeed'];
 
 if (substr($pulsoidUrl, 0, 32) == 'https://pulsoid.net/v1/api/feed/') {
-    $data = file_get_contents($pulsoidUrl);
+
+    $contextOptions = [
+        "ssl" => [
+            "verify_peer" => false,
+            "verify_peer_name" => false,
+        ]
+    ];
+
+    $data = file_get_contents($pulsoidUrl, false, stream_context_create($contextOptions));
     echo $data;
     exit();
 }
