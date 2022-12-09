73276dbfc88f97d346ad6af921544ba945e7f94f
UnskilledFreak <git@unskilledfreak.de>
2022-12-09 22:47:38 +0100

    DataPuller 2.1.0 ready

--- ts/src/Game/BeatSaber/Connection/DataPuller_2_1_0.ts
+++ ts/src/Game/BeatSaber/Connection/DataPuller_2_1_0.ts
@@ -6,7 +6,7 @@ namespace Freakylay.Game.BeatSaber.Connection {
         }
 
         public getName(): string {
-            return 'DataPuller 2.1.0 (WIP)';
+            return 'DataPuller 2.1.0';
         }
 
         protected setCompatibility(): void {
